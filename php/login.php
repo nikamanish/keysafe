ada836f52f4199f992ecc59153ed2aea2412b88d
Idyllic <nikamanish007@gmail.com>
2017-03-26 10:59:10 +0530

    Added jQuery CDN

--- php/login.php
+++ php/login.php
@@ -21,7 +21,7 @@
 
 
 	</body>
-    <script type="text/javascript" src="../js/jquery.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
     <script type="text/javascript" src="../js/calc.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js"></script>
     <script type="text/javascript">
